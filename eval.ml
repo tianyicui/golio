6763a97233ae483961c245ec173fcd0efa805451
Tianyi Cui <tianyicui@gmail.com>
2012-09-22 10:34:22 +0800

    added primitives for type-testing and symbol-handling

--- eval.ml
+++ eval.ml
@@ -1,5 +1,8 @@
 open Sexp
 
+module L = List
+module M = Map.Make(String)
+
 let unpackNum sexp =
     match sexp with
     | Number num -> num
@@ -7,22 +10,83 @@ let unpackNum sexp =
 ;;
 
 let binNumOp op params =
-    match (List.map unpackNum params) with
-    | hd :: tl -> Number (List.fold_left op hd tl)
+    match (L.map unpackNum params) with
+    | hd :: tl -> Number (L.fold_left op hd tl)
     | _ -> invalid_arg "binNumOp: expected at least 1 argument"
 ;;
 
-let primitives = [
-    "+", binNumOp (+);
-    "-", binNumOp (-);
-    "*", binNumOp ( * );
-    "/", binNumOp (/);
-    "%", binNumOp (mod);
-];;
+let symbolp arg =
+    match arg with
+    | Atom _ -> Bool true
+    | _ -> Bool false
+;;
+
+let numberp arg =
+    match arg with
+    | Number _ -> Bool true
+    | _ -> Bool false
+;;
+
+let stringp arg =
+    match arg with
+    | String _ -> Bool true
+    | _ -> Bool false
+;;
+
+let boolp arg =
+    match arg with
+    | Bool _ -> Bool true
+    | _ -> Bool false
+;;
+
+let listp arg =
+    match arg with
+    | List _ | DottedList _ -> Bool true
+    | _ -> Bool false
+;;
+
+let unaryOp op params =
+    match params with
+    | [x] -> op x
+    | xs -> invalid_arg (Printf.sprintf "unaryOp: expected 1 argument, given %i"
+                                        (L.length xs))
+;;
+
+let unpackSym sexp =
+    match sexp with
+    | Atom sym -> sym
+    | _ -> invalid_arg "unpackSym: expected a symbol"
+;;
+
+let unpackStr sexp =
+    match sexp with
+    | String str -> str
+    | _ -> invalid_arg "unpackStr: expected a string"
+;;
+
+let primitives =
+    L.fold_left
+        (fun m (k, v) -> M.add k v m)
+        M.empty
+        [
+            "+", binNumOp (+);
+            "-", binNumOp (-);
+            "*", binNumOp ( * );
+            "/", binNumOp (/);
+            "%", binNumOp (mod);
+            "symbol?", unaryOp symbolp;
+            "string?", unaryOp stringp;
+            "number?", unaryOp numberp;
+            "bool?", unaryOp boolp;
+            "list?", unaryOp listp;
+            "symbol->string", (fun p -> String (unaryOp unpackSym p));
+            "string->symbol", (fun p -> Atom (unaryOp unpackStr p));
+        ]
+;;
 
 let apply func args =
     let prim =
-        try List.assoc func primitives
+        try M.find func primitives
         with Not_found -> invalid_arg "apply: undefined function"
     in prim args
 ;;
@@ -31,7 +95,7 @@ let rec eval sexp =
     match sexp with
     | String _ | Number _ | Bool _ -> sexp
     | List [Atom "quote"; p] -> p
-    | List (Atom func :: args) -> apply func (List.map eval args)
+    | List (Atom func :: args) -> apply func (L.map eval args)
     | List _ -> invalid_arg "eval: invalid application"
     | DottedList _ -> invalid_arg "eval: cannot eval dotted list"
     | Atom _ -> failwith "not implemented"
--- repl_test.ml
+++ repl_test.ml
@@ -6,17 +6,21 @@ let _ =
     test "\"abc\"" "\"abc\"";
 
     test "'abc" "abc";
-
     test "'()" "()";
-
     test "'(compose f g)" "(compose f g)";
 
     test "1" "1";
-
     test "(+ 1 1)" "2";
-
     test "(+ 1 2 -3)" "0";
-
     test "(+ (* 3 4) (- -4 5) (/ 2 -1) (% 6 3))" "1";
+
+    test "(symbol? #f) (symbol? 'a)" "#f\n#t";
+    test "(string? 'a) (string? \"a\")" "#f\n#t";
+    test "(number? #f) (number? 1)" "#f\n#t";
+    test "(bool? 't) (bool? #t)" "#f\n#t";
+    test "(list? '()) (list? '(a)) (list? '(a b))" "#t\n#t\n#t";
+    test "(list? '(a . b)) (list? 2)" "#t\n#f";
+
+    test "(symbol->string 'a) (string->symbol \"a\")" "\"a\"\na";
 
     Printf.printf "All passed!\n"
