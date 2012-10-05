1146c33e33ba472491f710a22fd35dca117839e9
Tianyi Cui <tianyicui@gmail.com>
2012-10-05 17:08:59 +0800

    doc for Stdlib

--- src/stdlib.mli
+++ src/stdlib.mli
@@ -1,3 +1,7 @@
+(** The stdlib, the goal is as much [R5RS]-compatible as possible *)
+
+(** it is a [string] instead of a separate [.scm] file is because of simplified
+  * building, packing and install *)
 val stdlib :
   string
 ;;
