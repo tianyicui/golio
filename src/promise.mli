ebaa63aed2ce99250435b97161572ddcfa0ef611
Tianyi Cui <tianyicui@gmail.com>
2012-10-05 17:04:28 +0800

    doc for Promise

--- src/promise.mli
+++ src/promise.mli
@@ -1,13 +1,19 @@
+(** operations on {!Type.promise} *)
+
 open Type
 
+(** create a new {!Type.promise} from a function that will calculate its value *)
 val create :
   (unit -> value) -> promise
 ;;
 
+(** check whthere a {!Type.promise} has already been calculated *)
 val is_val :
   promise -> bool
 ;;
 
+(** get the {!Type.value} of a {!Type.promise}, will not re-calculate if already
+  * did *)
 val force :
   promise -> value
 ;;
