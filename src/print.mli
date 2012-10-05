d19b74aada6ddcce5cb19fd2d6f5d8f59b290d90
Tianyi Cui <tianyicui@gmail.com>
2012-10-05 17:00:55 +0800

    doc of Print

--- src/print.mli
+++ src/print.mli
@@ -1,13 +1,19 @@
+(** Represent various types as string *)
+
 open Type
 
+(** represent a {!Type.sexp} as [string] *)
 val print_sexp :
   sexp -> string
 ;;
 
+(** represent a {!Type.value} as [string] *)
 val print_value :
   value -> string
 ;;
 
+(** represent an exception as string, when it's not a kind of exception defined
+  * in {!Type}, it will use [Printexc.to_string]. *)
 val print_exn :
   exn -> string
 ;;
