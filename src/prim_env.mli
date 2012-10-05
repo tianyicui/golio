8e7a9300f309bf099beade13200773614c6c9b57
Tianyi Cui <tianyicui@gmail.com>
2012-10-05 16:37:24 +0800

    doc for Prim_env

--- src/prim_env.mli
+++ src/prim_env.mli
@@ -1,5 +1,10 @@
+(** The primitive environment *)
+
 open Type
 
+(** create the primitive {!Type.env}. It will load the primitives in
+  * {!Prim_func}, {!Prim_macro} and {!Prim_port} as global variables. Then it
+  * will evaluate {!Stdlib.stdlib}. *)
 val prim_env :
   unit -> env
 ;;
